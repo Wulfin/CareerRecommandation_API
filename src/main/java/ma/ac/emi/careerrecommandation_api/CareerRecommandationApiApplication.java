bb09951bd1de78af69be7b3b3922f3aeeb9a76c1
Saif <117396354+Wulfin@users.noreply.github.com>
2023-05-29 18:51:37 +0100

    Initial commit

---
+++ src/main/java/ma/ac/emi/careerrecommandation_api/CareerRecommandationApiApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.careerrecommandation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerRecommandationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerRecommandationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ac/emi/careerrecommandation_api/CareerRecommandationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.careerrecommandation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerRecommandationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
